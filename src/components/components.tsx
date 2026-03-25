dfad0a5f5a299d08ad96ce09219c0302ab1b27c9
William Cairns <cairnswm@gmail.com>
2026-03-25 13:25:59 +0200

    Add dynamic component wrapper file

---
+++ src/components/components.tsx
@@ -0,0 +1,41 @@
+import { DynamicComponent } from './ComponentRegistry';
+
+export const Hero = (props: any) => (
+  <DynamicComponent componentName="hero" props={props} />
+);
+
+export const Hero2 = (props: any) => (
+  <DynamicComponent componentName="hero2" props={props} />
+);
+
+export const Stats = (props: any) => (
+  <DynamicComponent componentName="stats" props={props} />
+);
+
+export const Companies = (props: any) => (
+  <DynamicComponent componentName="companies" props={props} />
+);
+
+export const Story = (props: any) => (
+  <DynamicComponent componentName="story" props={props} />
+);
+
+export const Testimonial = (props: any) => (
+  <DynamicComponent componentName="testimonial" props={props} />
+);
+
+export const BenefitsSection = (props: any) => (
+  <DynamicComponent componentName="benefitsSection" props={props} />
+);
+
+export const CTABanner = (props: any) => (
+  <DynamicComponent componentName="ctaBanner" props={props} />
+);
+
+export const FeaturesBanner = (props: any) => (
+  <DynamicComponent componentName="featuresBanner" props={props} />
+);
+
+export const WinnersBanner = (props: any) => (
+  <DynamicComponent componentName="winnersBanner" props={props} />
+);
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,21 +1,12 @@
-import { DynamicComponent } from '../components/ComponentRegistry';
+import { Hero, Stats, Companies } from '../components/components';
 import landingData from '../data/landing.json';
 
 export default function Landing() {
   return (
     <div>
-      <DynamicComponent
-        componentName="hero"
-        props={{ data: landingData.sections[0].data }}
-      />
-      <DynamicComponent
-        componentName="stats"
-        props={{ data: landingData.sections[1].data }}
-      />
-      <DynamicComponent
-        componentName="companies"
-        props={{ data: landingData.sections[2].data }}
-      />
+      <Hero data={landingData.sections[0].data} />
+      <Stats data={landingData.sections[1].data} />
+      <Companies data={landingData.sections[2].data} />
     </div>
   );
 }
